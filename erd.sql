a509b0a2e79a64b4b8a10ef15f912ad7c94e8a77
Jun <timeisonourside@gmail.com>
2015-02-07 16:48:26 -0800

    add ERD sql so u can use it on your db or remote db is already set

--- erd.sql
+++ erd.sql
@@ -8,22 +8,22 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema e_mall
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mydb` ;
+DROP SCHEMA IF EXISTS `e_mall` ;
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema e_mall
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `e_mall` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `e_mall` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`categories`
+-- Table `e_mall`.`categories`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`categories` ;
+DROP TABLE IF EXISTS `e_mall`.`categories` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`categories` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`categories` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
@@ -31,11 +31,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`products`
+-- Table `e_mall`.`products`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`products` ;
+DROP TABLE IF EXISTS `e_mall`.`products` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`products` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`products` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   `desc` VARCHAR(45) NULL,
@@ -50,18 +50,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`products` (
   INDEX `fk_products_categories1_idx` (`categories_id` ASC),
   CONSTRAINT `fk_products_categories1`
     FOREIGN KEY (`categories_id`)
-    REFERENCES `mydb`.`categories` (`id`)
+    REFERENCES `e_mall`.`categories` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`locations`
+-- Table `e_mall`.`locations`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`locations` ;
+DROP TABLE IF EXISTS `e_mall`.`locations` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`locations` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`locations` (
   `id` INT NOT NULL,
   `city_name` VARCHAR(255) NULL,
   `state_name` VARCHAR(45) NULL,
@@ -70,11 +70,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`billing_address`
+-- Table `e_mall`.`billing_address`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`billing_address` ;
+DROP TABLE IF EXISTS `e_mall`.`billing_address` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`billing_address` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`billing_address` (
   `id` INT NOT NULL,
   `address1` VARCHAR(255) NULL,
   `address2` VARCHAR(255) NULL,
@@ -83,18 +83,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`billing_address` (
   INDEX `fk_billing_address_locations1_idx` (`locations_id` ASC),
   CONSTRAINT `fk_billing_address_locations1`
     FOREIGN KEY (`locations_id`)
-    REFERENCES `mydb`.`locations` (`id`)
+    REFERENCES `e_mall`.`locations` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`ship_address`
+-- Table `e_mall`.`ship_address`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`ship_address` ;
+DROP TABLE IF EXISTS `e_mall`.`ship_address` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`ship_address` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`ship_address` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `address1` VARCHAR(255) NULL,
   `address2` VARCHAR(45) NULL,
@@ -107,23 +107,23 @@ CREATE TABLE IF NOT EXISTS `mydb`.`ship_address` (
   INDEX `fk_ship_address_billing_address1_idx` (`billing_address_id` ASC, `billing_address_locations_id` ASC),
   CONSTRAINT `fk_ship_address_locations1`
     FOREIGN KEY (`locations_id`)
-    REFERENCES `mydb`.`locations` (`id`)
+    REFERENCES `e_mall`.`locations` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_ship_address_billing_address1`
     FOREIGN KEY (`billing_address_id` , `billing_address_locations_id`)
-    REFERENCES `mydb`.`billing_address` (`id` , `locations_id`)
+    REFERENCES `e_mall`.`billing_address` (`id` , `locations_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`customers`
+-- Table `e_mall`.`customers`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`customers` ;
+DROP TABLE IF EXISTS `e_mall`.`customers` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`customers` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`customers` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NULL,
   `last_name` VARCHAR(45) NULL,
@@ -133,18 +133,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`customers` (
   INDEX `fk_customers_ship_address1_idx` (`ship_address_id` ASC, `ship_address_locations_id` ASC),
   CONSTRAINT `fk_customers_ship_address1`
     FOREIGN KEY (`ship_address_id` , `ship_address_locations_id`)
-    REFERENCES `mydb`.`ship_address` (`id` , `locations_id`)
+    REFERENCES `e_mall`.`ship_address` (`id` , `locations_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`orders`
+-- Table `e_mall`.`orders`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`orders` ;
+DROP TABLE IF EXISTS `e_mall`.`orders` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`orders` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`orders` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `item_count` VARCHAR(45) NULL,
   `created_at` DATETIME NULL,
@@ -153,18 +153,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`orders` (
   INDEX `fk_orders_customers1_idx` (`customers_id` ASC),
   CONSTRAINT `fk_orders_customers1`
     FOREIGN KEY (`customers_id`)
-    REFERENCES `mydb`.`customers` (`id`)
+    REFERENCES `e_mall`.`customers` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`orders_has_products`
+-- Table `e_mall`.`orders_has_products`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`orders_has_products` ;
+DROP TABLE IF EXISTS `e_mall`.`orders_has_products` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`orders_has_products` (
+CREATE TABLE IF NOT EXISTS `e_mall`.`orders_has_products` (
   `orders_id` INT NOT NULL,
   `products_id` INT NOT NULL,
   PRIMARY KEY (`orders_id`, `products_id`),
@@ -172,12 +172,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`orders_has_products` (
   INDEX `fk_orders_has_products_orders_idx` (`orders_id` ASC),
   CONSTRAINT `fk_orders_has_products_orders`
     FOREIGN KEY (`orders_id`)
-    REFERENCES `mydb`.`orders` (`id`)
+    REFERENCES `e_mall`.`orders` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_orders_has_products_products1`
     FOREIGN KEY (`products_id`)
-    REFERENCES `mydb`.`products` (`id`)
+    REFERENCES `e_mall`.`products` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
