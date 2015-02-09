e6e5b428e92f527801e7133a52223d84eb13abf5
Rui Wang <wangr3@gmail.com>
2015-02-08 22:47:31 -0800

    new feature payment

---
+++ application/controllers/carts.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class  extends CI_Controller {
+
+   public function __construct(){
+        parent::__construct();
+        $this->output->enable_profiler();
+    }
+
+    public function index(){
+         
+         $this->load->view("index" ) ;
+                
+    }
+}
+
