8e79fe005d210b46d9e9dfdf44928b34ce5713aa
Jun <timeisonourside@gmail.com>
2015-02-10 14:54:01 -0800

    add quantity in view for detailed product

--- application/views/product_detail.php
+++ application/views/product_detail.php
@@ -4,7 +4,13 @@
 <div><?= $product[0]['price']?></div>
 <p><?= $product[0]['description']?></p>
 <form method="post" action="/add/">
-	<input type="hidden" name="id" value="">
-	<input type="hidden" name="quantity" value="">
+	<input type="hidden" name="id" value="<?= $product[0]['id']?>">
+	<select name="quantity">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+	</select>
 	<button type="submit" class="btn btn-primary">Add Cart</button>
 </form>
