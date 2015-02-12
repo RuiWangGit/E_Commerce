7f944a676fcc9cc9a4a94741348addee5afebcbd
tmalik25 <tmalik.uiuc@gmail.com>
2015-02-11 16:43:27 -0800

    taron work

---
+++ application/views/ecommerce_v1/edit_product.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title></title>
+</head>
+
+<style type="text/css">
+
+.container{
+	max-width: 500px;
+	max-height: 1000px;
+	margin: 0px auto;
+}
+
+</style>
+
+<body>
+
+	<div class="container">
+
+		<form>
+			<input type='text' name='product_name' placeholder='Enter Product name' />
+		</form>
+
+	</div>
+
+</body>
+</html>
